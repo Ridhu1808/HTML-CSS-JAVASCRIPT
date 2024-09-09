55768fbd8204e12a9a589a748d9324293beb5f80
Ridhu1808 <ridhu.varshini@changecx.com>
2024-09-09 13:12:26 +0530

    Added Script and HTML

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import { Sample } from "./sample.js";
-// export const Hello = () => {
-//   // console.log("Hello, World!");
-//   // Sample();
-//   alert("HELLLLLLOOOOOOOOO");
-// };
-// Hello();
 window.Hello = function () {
-  alert("HELLOOO SAMPLE !!");
+  // console.log("Hello, World!");
+  // Sample();
+  alert("HELLLLLLOOOOOOOOO");
 };
+Hello();
+// window.Hello = function () {
+//   alert("HELLOOO SAMPLE !!");
+// };

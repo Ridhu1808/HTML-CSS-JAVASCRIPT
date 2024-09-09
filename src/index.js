02762596390e4b436eb997e640b2e378fcd7a2dc
Ridhu1808 <ridhu.varshini@changecx.com>
2024-09-09 15:59:08 +0530

    Added Script to HTML - Function Call

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-// import { Sample } from "./sample.js";
+import { Sample } from "./sample.js";
 window.Hello = function () {
-  // console.log("Hello, World!");
-  // Sample();
-  alert("HELLLLLLOOOOOOOOO");
+  console.log("Hello, World!");
+  Sample();
+  // alert("HELLLLLLOOOOOOOOO");
 };
 Hello();
 // window.Hello = function () {
